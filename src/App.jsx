9c75d7e286313fadb8d8088ee2c9574363132cfd
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-08-08 22:37:07 +0300

    התחלה עם המרה הסיפריות אבל עדיין לא רואה את הנקודה האדומה

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
 
-import './App.css'
+const tabs = [
+  { id: "world", label: "World" },
+  { id: "ny", label: "N.Y." },
+  { id: "business", label: "Business" },
+  { id: "arts", label: "Arts" },
+  { id: "science", label: "Science" }
+];
 
-function App() {
-
+export default function AnimatedTabs() {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
 
   return (
-    <>
-      <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col items-center">
+      <div className="flex space-x-1 justify-center">
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`${
+              activeTab === tab.id ? "bg-blue-500" : "hover:opacity-50"
+            } rounded-full px-3 py-1.5 text-sm font-medium text-white outline-2 outline-sky-400 focus-visible:outline`}
+          >
+            {tab.label}
+          </button>
+        ))}
       </div>
-    </>
-  )
-}
 
-export default App
+      <div className="mt-4 flex justify-center">
+        <div className="h-5 w-5 rounded-full bg-red-500" />
+      </div>
+    </div>
+  );
+}
